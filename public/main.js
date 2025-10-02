bf0575b75829ac83d55b794e8200bf1c133c94e1
Lan5555 <okekejohnson24@gmail.com>
2025-10-02 12:13:56 +0100

    update

--- public/main.js
+++ public/main.js
@@ -4,7 +4,7 @@ const result = document.querySelector('.result');
 button.addEventListener('click', async() => {
     try{
         const res = await fetch('/api/get-hidden-item');
-        const data = res.text();
+        const data = await res.text();
         result.innerHTML = data;
     }catch(e){
 
